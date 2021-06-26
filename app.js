5b833a098e73298274ffc9878eb79c361814771e
rachitpanwar151 <79990541+rachitpanwar151@users.noreply.github.com>
2021-06-26 23:15:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import footer from 'footer';
+import Info from "info";
+function App(){
+	return(
+		<div>
+		<header />
+		<footer />
+		<info />
+		<info />
+		<info />
+		<info />
+		</div>
+		);
+	}
+	export default App;
